dc324941a6da85d8b62e737af922da1b645dccae
Karl Dyson <bitbucket@karl.dyson.uk.net>
2020-12-14 10:26:42 +0000

    tweaked version of randomsparkles.py that slows the rate down and generally brings the colour hue down. also added random brightness.

---
+++ randomsparkles.py
@@ -0,0 +1,20 @@
+from tree import RGBXmasTree
+import random
+import time
+
+tree = RGBXmasTree()
+
+def random_color():
+    r = random.random() ** 3
+    g = random.random() ** 3
+    b = random.random() ** 3
+    return (r, g, b)
+
+try:
+    while True:
+        pixel = random.choice(tree)
+        pixel.color = random_color()
+        pixel.brightness = random.random() ** 3
+        time.sleep(0.20)
+except KeyboardInterrupt:
+    tree.close()
